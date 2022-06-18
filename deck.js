311e1e7d8c21ce74df3b057b246eccad57d7e573
AK77 <80969885+ak77-script@users.noreply.github.com>
2022-06-18 18:48:28 +0300

    update

--- deck.js
+++ deck.js
@@ -3,13 +3,14 @@
  */
 
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Guide/Functions
+// https://ru.vuejs.org/v2/guide/index.html
 
 'use strict';
  
 var memodeck = (function (onoff) {
 	'use strict';
 	var containerId = 'cntnr';
-	var ss = '';
+	var getstartbtnId = 'getStart';
 	
 	function memodeck() {
 
