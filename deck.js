7fd540e7a10ef9090926832dd9d4c8f636243302
AK77 <80969885+ak77-script@users.noreply.github.com>
2022-06-19 21:10:48 +0300

    update deck.js

--- deck.js
+++ deck.js
@@ -17,12 +17,18 @@ let memodeck = (function (onoff) {
 		// инициализация
 		let self = this;
 		self.d = _dom;
+
 		const _body = seld.d.select('body');
 		const _header = _body.select('header');
 		const _nav = _body.select('nav');
 		const _main = _body.select('main');
 		const _footer = _body.select('footer');
 		
+		self.body = _body;
+		self.header = _header;
+		self.nav = _nav;
+		self.main = _main;
+		self.footer = _footer;
 		
 		return self;
 	}
